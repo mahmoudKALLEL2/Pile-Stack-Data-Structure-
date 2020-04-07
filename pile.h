0642ca5b5fa4b7cb3bbc040c708334ab5733e5b0
mahmoudKALLEL2 <63267557+mahmoudKALLEL2@users.noreply.github.com>
2020-04-07 14:32:50 +0100

    Create pile.h

---
+++ pile.h
@@ -0,0 +1,37 @@
+/*
+ * test.h
+ *
+ *  Created on: 5 avr. 2020
+ *      Author: USER
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+typedef enum{
+	false,
+	true
+} Bool;
+
+
+typedef struct Element Element;
+struct Element {
+	int val;
+	struct Element *suivant;
+};
+
+typedef struct Pile Pile;
+struct Pile{
+	struct Element *premier;
+};
+
+Pile *initialiser(void);
+Bool pile_estvide(Pile *pile);
+void empiler(Pile *pile, int val);
+int depiler(Pile *pile);
+void afficher(Pile *pile);
+void taille_pile(Pile *pile, int *nbrElt);
+void reset(Pile *pile);
+
+#endif /* TEST_H_ */
